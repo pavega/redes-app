e9e96d1ebbf7c29c57babb79db6635ae2cdafd57
pavega <vega.vasquezpao@gmail.com>
2022-10-28 01:58:45 -0600

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -7,7 +7,7 @@ module.exports = function (config) {
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
-      require('karma-chrome-launcher'),
+      //require('karma-chrome-launcher'),
       require('karma-phantomjs-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
@@ -38,7 +38,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['PhantomJS', 'Chrome', 'ChromeHeadless'],
+    browsers: ['PhantomJS'],// 'Chrome', 'ChromeHeadless'],
     singleRun: false,
     restartOnFileChange: true
   });
